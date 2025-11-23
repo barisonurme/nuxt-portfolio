b3ae4544ba0addf2dc4524ebd5484a16c4548fd8
barisonurme <barisonurme@users.noreply.github.com>
2025-11-24 00:07:01 +0300

    feat: contact added to about page and headline corrected

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -3,9 +3,16 @@
     <div class="flex flex-col w-full max-w-7xl h-full">
         <UPageHeader :ui="{ container: 'px-4', headline: 'px-4' }" class="flex flex-col w-full" title="About Me"
             description="Have questions or want to collaborate? Send me a message, and I'll get back to you soon!"
-            headline="About me" />
+            headline="Information" />
 
         <AboutView :page-mode="true" />
+
+        <NuxtLink class="pl-8 pb-4" to="/contact">
+            <p class="group text-2xl font-semibold hover:underline cursor-pointer items-center flex">
+                Contact
+                <UIcon name="i-lucide-chevron-right" class="size-5 group-hover:translate-x-1/2 duration-500" />
+            </p>
+        </NuxtLink>
     </div>
 </template>
 <script lang="ts" setup>
