ede4c715339c8b5668037a94760f6941f0cd689d
barisonurme <barisonurme@users.noreply.github.com>
2026-02-24 17:16:27 +0300

    feat: test page moved to header for testing

--- app/components/ui/container-text-flip/ContainerTextFlip.vue
+++ app/components/ui/container-text-flip/ContainerTextFlip.vue
@@ -1,7 +1,7 @@
 <template>
   <Motion :key="words[currentWordIndex]" as="p" :layout-id="`words-here-${id}`" :animate="{ width }"
     :transition="{ duration: props.animationDuration / 2000 }" :class="cn(
-      'relative inline-block rounded-lg pt-2 pb-3 xl:px-4 text-center text-6xl font-bold text-black xl:text-[102px] dark:text-white',
+      'relative inline-block rounded-lg pt-2 pb-3 xl:px-4 text-center text-6xl font-bold text-black xl:text-7xl dark:text-white',
       props.class,
     )
       ">
@@ -40,7 +40,7 @@ const props = withDefaults(
     textClass?: string;
   }>(),
   {
-    words: () => ["better", "modern", "beautiful", "awesome"],
+words: () => ["that matter", "that scale", "with intention", "with precision"],
     interval: 3000,
     animationDuration: 700,
   },
